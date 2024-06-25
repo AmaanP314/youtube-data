f422957de19f1f13fde6e38d85ab023c7315f7b5
Amaan Poonawala <amaanpoonawala05@gmail.com>
2024-06-25 18:48:57 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,36 @@
+
+from flask import Flask, request, render_template, redirect, url_for
+from youtube_search import search_youtube
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/search', methods=['POST'])
+def search():
+    query = request.form['query']
+    sort_by = request.form['sort_by']
+    max_results = int(request.form['max_results'])
+    return redirect(url_for('results', query=query, sort_by=sort_by, max_results=max_results))
+
+@app.route('/results')
+def results():
+    query = request.args.get('query')
+    sort_by = request.args.get('sort_by')
+    max_results = int(request.args.get('max_results'))
+    data = search_youtube(query, sort_by=sort_by, max_results=max_results)
+    if data is None:
+        return render_template('results.html', query=query, error="No data found.")
+    
+    df, total_plot, percent_plot, engagement_rate_plot, composite_score_plot = data
+    df_html = df.to_html(classes='table table-striped', index=False, escape=False)
+    
+    return render_template('results.html', query=query, table=df_html, total_plot=total_plot, percent_plot=percent_plot, engagement_rate_plot=engagement_rate_plot, composite_score_plot=composite_score_plot)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
