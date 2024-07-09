e1a7090d7e6d8f3a7af178649aa0992bb4adc8b0
Amaan Poonawala <amaanpoonawala05@gmail.com>
2024-07-09 15:56:06 +0530

    major changes

--- youtube_search.py
+++ youtube_search.py
@@ -8,12 +8,13 @@ from matplotlib.ticker import PercentFormatter, FuncFormatter
 import warnings
 import io
 import base64
+import re
 matplotlib.use('Agg')
 
 warnings.filterwarnings("ignore", module="matplotlib")
 sns.set()
 
-api_key = 'AIzaSyDtqLe7rRVVuYq3HsjbLOov-3mf_ZI2Mlg'
+api_key = 'YOUR API KEY'
 
 youtube = build('youtube', 'v3', developerKey = api_key)
 
@@ -189,4 +190,16 @@ def plot_to_base64(plot_func, df, plot_type):
     plt.close()
     img.seek(0)
     return base64.b64encode(img.getvalue()).decode('utf8')
+
+def strip_emojis(text):
+    emoji_pattern = re.compile(
+        "[" u"\U0001F600-\U0001F64F" 
+        u"\U0001F300-\U0001F5FF"  
+        u"\U0001F680-\U0001F6FF"  
+        u"\U0001F1E0-\U0001F1FF" 
+        u"\U00002702-\U000027B0"
+        u"\U000024C2-\U0001F251"
+        "]+", flags=re.UNICODE)
+    return emoji_pattern.sub(r'', text)
+
 
